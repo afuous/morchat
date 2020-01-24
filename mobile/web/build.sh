347c27cbeb306a024229f5d399b100dd7b924c4a
afuous <ben@bheller.me>
2020-01-23 16:24:01 -0800

    smh

--- mobile/web/build.sh
+++ mobile/web/build.sh
@@ -8,27 +8,27 @@ dir="$(dirname "$0")"
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1" />'
 	echo '<style type="text/css">'
 	# curl -s 'https://fonts.googleapis.com/css?family=Exo+2:400,300,200'
-	cat $dir/css/google-fonts.css
-	cat $dir/css/shared.css
-	cat $dir/css/login.css
-	cat $dir/css/chatlist.css
-	cat $dir/css/chat.css
+	cat "$dir/css/google-fonts.css"
+	cat "$dir/css/shared.css"
+	cat "$dir/css/login.css"
+	cat "$dir/css/chatlist.css"
+	cat "$dir/css/chat.css"
 	echo '</style>'
 	echo '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>'
 	echo '<script type="text/javascript">'
-	# cat $dir/js/socket.io.js
+	# cat "$dir/js/socket.io.js"
 	# echo
-	cat $dir/js/shared.js
+	cat "$dir/js/shared.js"
 	echo
-	cat $dir/js/login.js
+	cat "$dir/js/login.js"
 	echo
-	cat $dir/js/chatlist.js
+	cat "$dir/js/chatlist.js"
 	echo
-	cat $dir/js/chat.js
+	cat "$dir/js/chat.js"
 	echo
-	cat $dir/js/entry.js
+	cat "$dir/js/entry.js"
 	echo '</script>'
 	echo '</head>'
 	echo '<body></body>'
 	echo '</html>'
-} > build.html
+} > "$dir/build.html"
