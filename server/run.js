d7ebb3d5f0f5fc3d5fb4582ef9089f6a12ef35fc
afuous <ben@bheller.me>
2020-02-09 01:23:22 -0800

    Add socketio CORS handler

--- server/run.js
+++ server/run.js
@@ -8,5 +8,15 @@ let server = http.createServer((req, res) => {
     morchat.app(req, res);
 });
 server.listen(process.argv[2] || 80);
-let io = socketio.listen(server);
+let io = socketio.listen(server, {
+    // https://github.com/socketio/socket.io-client/issues/1140#issuecomment-325958737
+    handlePreflightRequest: (req, res) => {
+        res.writeHead(200, {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": "*",
+        });
+        res.end();
+    },
+});
+
 io.on("connection", morchat.sioOnConnection);
